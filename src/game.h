263dd230570bbd515a7f20db4b025add3ef89305
fganga <fganga@unaj.edu.ar>
2024-02-12 21:43:19 -0300

    Initial commit

---
+++ src/game.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "entity.h"
+#include "entity_manager.h"
+
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+    sf::RenderWindow m_window;
+    EntityManager m_entities;
+    sf::Font m_font;
+    sf::Text m_text;
+    sf::Clock m_deltaClock;
+    int m_score = 0;
+    int m_currentFrame = 0;
+    bool m_running = true;
+    std::shared_ptr<Entity> m_player;
+
+    void init();
+    void spawnPlayer();
+    void sEnemySpawner();
+    void spawnSmallEnemies(std::shared_ptr<Entity> e);
+    void spawnBullet(std::shared_ptr<Entity> e, const Vec2& target);
+    void spawnSpecialWeapon(std::shared_ptr<Entity> e);
+    void sMovement();
+    void sLifespan();
+    void sCollision();
+    void sUserInput();
+    void sGUI();
+
+    void sRender();
+    void sInput();
+    public:
+    Game();
+    void run();
+
+};
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "game.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+#include <imgui-SFML.h>
+#include <imgui.h>
+
+int main() {
+    Game g;
+    g.run();
+}
